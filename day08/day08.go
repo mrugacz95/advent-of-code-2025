50a4c5849609905bb535211dfaeb00b841e55f99
marcin.mrugas <marcin.mrugas@allegro.com>
2025-12-12 15:05:07 +0100

    Template and placeholder for day 8

---
+++ day08/day08.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"advent-of-code-2025/utils"
+	"strings"
+
+	"github.com/Olegas/goaocd"
+)
+
+func parse(input string) []string {
+	return strings.Split(strings.TrimRight(input, "\n"), "\n")
+}
+
+func part1(inputData string) int {
+	//var input = parse(inputData)
+
+	return 0
+}
+
+func part2(inputData string) int {
+	//var input = parse(inputData)
+	return 0
+}
+
+const day = 8
+const year = 2025
+
+func main() {
+	var testInput = `123`
+	var input = goaocd.Input(year, day)
+	utils.Assert(-1, part1(testInput))
+	goaocd.Submit(1, part1(input), year, day)
+	utils.Assert(-1, part2(testInput))
+	goaocd.Submit(2, part2(input), year, day)
+}
